4c8783f67611f9ff5d12a17fde86d7c74a62f599
Raamishshah010 <raamishshah010@gmail.com>
2023-09-04 18:38:54 +0500

    Working on fribbble navbar

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import Nav from '@components/Nav'
 export default function Home() {
   return (
     <main className="">
+        <header className="bg-[#fafafa] px-12 pt-10 h-screen">
         <Nav/>
+        </header>
     </main>
   )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Nav = () => {
     return (
-        <nav className="px-12 mt-12 flex justify-between items-center">
+        <nav className=" flex justify-between items-center">
             <ul className="flex items-center gap-6 text-[19px] font-medium font-poppins">
                 <li>
                     <Link href='/'>Find talent</Link>
@@ -25,10 +25,19 @@ const Nav = () => {
                 <h1 className="font-pacifico text-4xl">Fribbble</h1>
             </div>
 
-            <div className="flex-1 flex justify-end">
-                <Link href="/login">
-                    Login
+            <div className="flex-1 flex gap-7 justify-center items-center font-poppins">
+                <Link href="/login" className="text-black px-8 py-3 bg-transparent font-semibold rounded-full">
+                    Log in <span className="text-2xl">.</span>
                 </Link>
+
+                <Link href="/register" className="text-black px-8 py-3 bg-white font-semibold rounded-full">
+                    Sign Up
+                </Link>
+
+                <Link href="/register" className="bg-black px-8 py-3 text-white font-semibold rounded-full">
+                    Hire Creatives
+                </Link>
+
             </div>
         </nav>
     )
